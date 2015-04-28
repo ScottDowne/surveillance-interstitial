0700381ddbc4e400feda74786c5a8cbcc7ec7ce1
Scott <scott@localhost.localdomain>
2015-04-28 19:49:19 -0400

    r

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express(),
   env = new Habitat();
 
 app.configure(function() {
-  app.use(express.static(__dirname));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.listen(env.get('PORT'), function () {
